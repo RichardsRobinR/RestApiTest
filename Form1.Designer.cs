49f7d4cc7372bc7b08eb8370b7aeece84b2e37db
RichardsRobinR <richardsrobin.r15@gmail.com>
2021-07-12 15:35:58 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace RestApiTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(672, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(546, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 355);
+            this.listBox1.TabIndex = 2;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(368, 208);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 94);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(89, 79);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(218, 264);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Urls;
+using RestClient.Net;
+
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+namespace RestApiTest
+{
+    public partial class Form1 : Form
+    {
+        private const string V = "https://restcountries.eu/rest/v2";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //getpokemon();
+            Graphics g = Graphics.FromHwnd(this.Handle);
+
+
+
+           this.imageList1.Images.Add(Image.FromFile(@"C:\Users\richa\Downloads\3.jpg"));
+
+            this.imageList1.ImageSize = new Size(256, 256);
+
+
+            //this.imageList1.Draw(g, new Point(40, 40),0);
+
+            //this.pictureBox1.Image = this.imageList1.Images[0];
+
+            //this.listBox1.Items.Add(this.imageList1.Images[0]);
+
+            Bitmap bitmap = new Bitmap(@"C:\Users\richa\Downloads\3.jpg");
+
+            this.listView1.LargeImageList.Images.Add(Image.FromFile(@"C:\Users\richa\Downloads\3.jpg"));
+
+            this.pictureBox1.Image = bitmap;
+
+        }
+
+
+        public async void getdata()
+        {
+           // var response = await "https://restcountries.eu/rest/v2".ToAbsoluteUrl;
+
+
+            var client = new HttpClient { BaseAddress = new Uri("https://restcountries.eu/") };
+
+            // Assign default header (Json Serialization)
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Make an API call and receive HttpResponseMessage
+            var responseMessage = await client.GetAsync("rest/v2", HttpCompletionOption.ResponseContentRead);
+
+            // Convert the HttpResponseMessage to string
+            var resultArray = await responseMessage.Content.ReadAsStringAsync();
+
+
+           
+            // Deserialize the Json string into type using JsonConvert
+            var personList = JsonConvert.DeserializeObject<List<RestCountry>>(resultArray);
+
+
+            for (int i = 0; i < personList.Count; i++)
+            {
+                this.listBox1.Items.Add(personList[i].capital);
+                //this.listBox1.Text = personList[0].name;
+            }
+            //this.listBox1.Items.Add(personList[0].name);
+            //this.listBox1.Text = personList[0].name;
+        }
+
+        public async void getpokemon()
+        {
+            var client = new HttpClient { BaseAddress = new Uri("https://pokeapi.co/api/v2/") };
+
+            // Assign default header (Json Serialization)
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Make an API call and receive HttpResponseMessage
+            var responseMessage = await client.GetAsync("pokemon?offset=0&limit=100", HttpCompletionOption.ResponseContentRead);
+
+            // Convert the HttpResponseMessage to string
+            var resultArray = await responseMessage.Content.ReadAsStringAsync();
+
+
+
+            // Deserialize the Json string into type using JsonConvert
+            var pokemonList = JsonConvert.DeserializeObject<Pokedex>(resultArray);
+
+
+            for (int i = 0; i < pokemonList.Results.Count; i++)
+
+                //var pokename = pokemonList[0].Next
+            {
+                this.listBox1.Items.Add(pokemonList.Results[i].Name);
+                //this.listBox1.Text = personList[0].name;
+            }
+            //this.listBox1.Items.Add(personList[0].name);
+            //this.listBox1.Text = personList[0].name;
+        }
+    
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<RestCountry> testcountry = new List<RestCountry>();
+
+
+            //RestCountry restCountry = testcountry[0].Name as RestCountry;
+
+            //this.textBox1.Text = per;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class RestCountry
+    {
+        public string name { get; set; }
+
+
+        public List<string> topLevelDomain { get; set; }
+        public string alpha2Code { get; set; }
+        public string alpha3Code { get; set; }
+        public List<string> callingCodes { get; set; }
+        public string capital { get; set; }
+        public List<object> altSpellings { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+        public int population { get; set; }
+        public List<object> latlng { get; set; }
+        public string demonym { get; set; }
+        public double? area { get; set; }
+        public double? gini { get; set; }
+        public List<string> timezones { get; set; }
+        public List<object> borders { get; set; }
+        public string nativeName { get; set; }
+        public string numericCode { get; set; }
+       // public List<Currency> currencies { get; set; }
+        //public List<Language> languages { get; set; }
+       // public Translations translations { get; set; }
+        public string flag { get; set; }
+        public List<object> regionalBlocs { get; set; }
+        public string cioc { get; set; }
+    }
+
+
+
+
+    //
+    // To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+    //
+   
+
+
+        public class Pokedex
+        {
+            
+            public long Count { get; set; }
+
+          
+            public Uri Next { get; set; }
+
+           
+            public Uri Previous { get; set; }
+
+            
+            public List<Result> Results { get; set; }
+        }
+
+        public  class Result
+        {
+            
+            public string Name { get; set; }
+
+           
+            public Uri Url { get; set; }
+        }
+
+        
+    
+
+}
